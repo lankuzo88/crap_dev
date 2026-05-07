c9d58bf54faca39c9aaa322fb3be9e8f339afd93
lankuzo88 <lankuzo@gmail.com>
2026-05-08 03:35:39 +1400

    fix: trust reverse proxy headers

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ const r2Client = new S3Client({
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
